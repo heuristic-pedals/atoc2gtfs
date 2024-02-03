087325c2ad5b39a6f5f97b6315d443fde27f5fe1
Heuristic Pedals <heuristicpedals@gmail.com>
2024-02-03 17:06:52 +0000

    feat: convert args to paths

--- src/cli.rs
+++ src/cli.rs
@@ -1,22 +1,25 @@
 use std::cmp::Ordering;
+use std::path::Path;
 
 /// Capture and collect the runtime configuration
-pub struct Config {
+pub struct Config<'a> {
     /// path to the input file
-    pub input_path: String,
+    pub input_path: &'a Path,
     /// path to write output file
-    pub output_path: String,
+    pub output_path: &'a Path,
 }
 
-impl Config {
+impl<'a> Config<'a> {
     pub fn build(parsed_args: &Vec<String>) -> Result<Config, &'static str> {
         match parsed_args.len().cmp(&3) {
             Ordering::Greater => return Err("Too many argument provided."),
             Ordering::Less => return Err("Too few arguments provided."),
             Ordering::Equal => (),
         }
-        let input_path: String = parsed_args[1].clone();
-        let output_path: String = parsed_args[2].clone();
+
+        let input_path: &Path = Path::new(&parsed_args[1]);
+        let output_path: &Path = Path::new(&parsed_args[2]);
+
         Ok(Config {
             input_path,
             output_path,
