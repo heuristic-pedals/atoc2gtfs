891648dbdc391945e95a3bd939c75dd3a440619d
Heuristic Pedals <heuristicpedals@gmail.com>
2024-02-03 18:24:24 +0000

    feat: added input defences

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,5 @@
 use std::cmp::Ordering;
+use std::ffi::OsStr;
 use std::path::Path;
 
 /// Capture and collect the runtime configuration
@@ -11,6 +12,9 @@ pub struct Config<'a> {
 
 impl<'a> Config<'a> {
     pub fn build(parsed_args: &Vec<String>) -> Result<Config, &'static str> {
+
+        const ACCEPT_ZIP_EXTS: [&str; 2] = ["zip", "ZIP"];
+
         match parsed_args.len().cmp(&3) {
             Ordering::Greater => return Err("Too many argument provided."),
             Ordering::Less => return Err("Too few arguments provided."),
@@ -19,6 +23,20 @@ impl<'a> Config<'a> {
 
         let input_path: &Path = Path::new(&parsed_args[1]);
         let output_path: &Path = Path::new(&parsed_args[2]);
+
+        if !input_path.exists() {
+            return Err("Provided input path does not exist.")
+        }
+        if !input_path.is_file() {
+            return Err("Provided input is not a file.")
+        }
+        
+        match input_path.extension().and_then(OsStr::to_str) {
+            Some(ext) => if !ACCEPT_ZIP_EXTS.contains(&ext) {
+                return Err("Provided input is not a zip file.")
+            },
+            None => return Err("Unable to determine the input file extension."),
+        };
 
         Ok(Config {
             input_path,
