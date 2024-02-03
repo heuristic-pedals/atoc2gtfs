043a6f82fc873dd3b13f78b1ee11391b1c7ac165
Heuristic Pedals <heuristicpedals@gmail.com>
2024-02-03 19:10:52 +0000

    feat: added conversion start msg

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ use std::process;
 fn main() {
     // collect command line arguments
     let args: Vec<String> = env::args().collect();
-    let _config: cli::Config = cli::Config::build(&args).unwrap_or_else(|err| {
+    let config: cli::Config = cli::Config::build(&args).unwrap_or_else(|err| {
         eprintln!("Error parsing arguments: {}", err);
         process::exit(1);
     });
+
+    println!("Converting {:?} to GTFS...", config.input_path)
 }
