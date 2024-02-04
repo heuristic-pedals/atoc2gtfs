78f9a594e82f818b6dcda4954b928fa008069825
Heuristic Pedals <heuristicpedals@gmail.com>
2024-02-04 16:22:01 +0000

    test: added check_zip unit tests

--- src/utils/io.rs
+++ src/utils/io.rs
@@ -14,3 +14,48 @@ pub fn check_zip(path: &Path) -> Result<(), String> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::Path;
+
+    #[test]
+    fn check_zip_on_pass() {
+        let dummy_inputs: [&Path; 2] = [
+            Path::new("./tests/data/dummy_empty.zip"),
+            Path::new("./tests/data/dummy_empty_capital.ZIP"),
+        ];
+        for dummy_input in dummy_inputs {
+            assert!(check_zip(&dummy_input).is_ok());
+        }
+    }
+
+    #[test]
+    fn check_zip_not_a_zip() {
+        let text_input = Path::new("./tests/data/dummy_empty.txt");
+        let result = check_zip(&text_input);
+        assert!(
+            result.is_err(),
+            "Did not raise error when provided text input."
+        );
+        assert!(
+            result.is_err_and(|err| err.contains("is not a zip file")),
+            "Unexpected error message when passing a text file as an input."
+        )
+    }
+
+    #[test]
+    fn check_zip_no_file_extension() {
+        let no_file_ext = Path::new("./tests/data/dummy_empty");
+        let result = check_zip(&no_file_ext);
+        assert!(
+            result.is_err(),
+            "Did not raise error when input with no extension was provided."
+        );
+        assert!(
+            result.is_err_and(|err| err.contains("Unable to determine file extension")),
+            "Unexpected error message when passing an input with no file extension."
+        )
+    }
+}
