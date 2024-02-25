fcbce15e5900263b0071975ccde2a1603b161d1f
Heuristic Pedals <heuristicpedals@gmail.com>
2024-02-25 16:01:58 +0000

    feat: added run() infrastructure

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,27 @@
 
 pub mod setup;
 pub mod utils;
+
+use setup::Config;
+use std::error::Error;
+
+/// Execute the complete `atoc2gtfs` pipeline using the [Config] setup:
+/// 1. Unzip ATOC input and check for expected contents.
+///
+/// # Arguments
+///
+/// - `config`: An instance of [Config].
+///
+/// # Example
+/// ```
+/// use atoc2gtfs::{setup::Config, run};
+/// let input_atoc_path = "./tests/data/dummy_empty.zip";                    // input ATOC.CIF path
+/// let output_gtfs_path = "./data/output_gtfs.zip";                         // output GTFS path
+/// let config = Config::new(&input_atoc_path, &output_gtfs_path).unwrap();  // setup a new Config
+/// assert!(run(config).is_ok())                                             // run the pipeline
+/// ```
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    println!("Converting {:?} to GTFS...", config.input_path);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-use atoc2gtfs::setup;
+use atoc2gtfs::{run, setup::Config};
 use std::env;
 use std::process;
 
 fn main() {
     // collect command line arguments
     let args: Vec<String> = env::args().collect();
-    let config: setup::Config = setup::Config::build_from_cli(&args).unwrap_or_else(|err| {
-        eprintln!("Error parsing arguments: {}", err);
+    let config: Config = Config::build_from_cli(&args).unwrap_or_else(|err| {
+        eprintln!("Error parsing arguments: {:?}", err);
         process::exit(1);
     });
 
-    println!("Converting {:?} to GTFS...", config.input_path)
+    // execute atoc2gtfs to convert input to GTFS
+    if let Err(e) = run(config) {
+        eprintln!("atoc2gtfs error: {:?}", e);
+        process::exit(1);
+    }
 }
